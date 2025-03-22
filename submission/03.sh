4659ee6fadd2705f9828496f92dd14a7dbaa7b93
Maazi Emeka <maazigithub@gmail.com>
2025-03-22 15:02:53 -0400

    Add: P2WPKH

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,7 @@
 # Created a SegWit address.
+SEGWIT_ADDR=$(bitcoin-cli -regtest -rpcwallet="btrustwallet" getnewaddress "" "bech32" )
 # Add funds to the address.
+bitcoin-cli -regtest generatetoaddress 101 $SEGWIT_ADDR &> /dev/null
+
 # Return only the Address
+echo "$SEGWIT_ADDR"
